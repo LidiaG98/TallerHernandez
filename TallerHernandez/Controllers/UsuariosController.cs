338a614dba8bd04aa1e18dc155d4f58bf4938f4b
Revenge <phoenixwright1998@gmail.com>
2020-09-21 15:25:27 -0600

    Comienzo de administración de usuarios

---
+++ TallerHernandez/Controllers/UsuariosController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TallerHernandez.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public UsuariosController(RoleManager<IdentityRole> roleManager, UserManager<IdentityUser> userManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var users = _userManager.Users;
+            return View(users);
+        }
+    }
+}
